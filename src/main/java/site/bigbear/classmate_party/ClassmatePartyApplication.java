95676e489635aca71fd15eae8e476658c8a52041
ytche <cheyantao@foxmail.com>
2018-02-23 15:18:55 +0800

    init

---
+++ src/main/java/site/bigbear/classmate_party/ClassmatePartyApplication.java
@@ -0,0 +1,12 @@
+package site.bigbear.classmate_party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmatePartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmatePartyApplication.class, args);
+	}
+}
